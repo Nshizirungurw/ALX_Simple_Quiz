5cba0c5bd1e5deaf5e08aad776e0b1f519848d17
Shema <nshizirungushizon@gmail.com>
2025-01-19 10:43:58 +0200

    initial commit

--- calculator.js
+++ calculator.js
@@ -3,23 +3,27 @@ document.getElementById('add').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = number1 + number2;
-    document.getElementById('calculation-result').textContent = result;
+    const displayResult =  document.getElementById('calculation-result').textContent = result;
+    return displayResult;
 }); 
 document.getElementById('subtract').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = number1 - number2;
-    document.getElementById('calculation-result').textContent = result;
+    const displayResult =  document.getElementById('calculation-result').textContent = result;
+    return displayResult;
 }); 
 document.getElementById('multiply').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = number1 * number2;
-    document.getElementById('calculation-result').textContent = result;
+    const displayResult =  document.getElementById('calculation-result').textContent = result;
+    return displayResult;
 }); 
 document.getElementById('divide').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = number1 / number2;
-    document.getElementById('calculation-result').textContent = result;
+    const displayResult =  document.getElementById('calculation-result').textContent = result;
+    return displayResult;
 }); 
